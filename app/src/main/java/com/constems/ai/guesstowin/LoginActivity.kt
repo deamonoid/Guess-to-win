e38c54d165783a581b5d2642da8a2fb6a28b8720
Tanmay Srivastava <tansrivastava@gmail.com>
2018-07-09 12:10:25 +0530

    Add LogIn implementation

--- app/src/main/java/com/constems/ai/guesstowin/LoginActivity.kt
+++ app/src/main/java/com/constems/ai/guesstowin/LoginActivity.kt
@@ -1,15 +1,65 @@
 package com.constems.ai.guesstowin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import org.jetbrains.anko.longToast
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
+
+    private var tag = LoginActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         setSupportActionBar(toolbar_loginActivity)
+
+        button_loginActivity_logIn.setOnClickListener {
+            getPostRequest("new", "pass")
+        }
+    }
+
+    private fun getPostRequest(emailId: String, password: String) {
+
+        val client = OkHttpClient()
+        val body = FormBody.Builder()
+                .add("emailId", emailId)
+                .add("password", password)
+                .build()
+
+        val request = Request.Builder()
+                .url("https://192.168.1.82/login_script/login.php")
+                .post(body)
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call?, e: IOException?) {
+                val message = e?.message.toString()
+                Log.w(tag, "Failure Response: $message")
+
+                this@LoginActivity.runOnUiThread {
+                    longToast("Please check your internet Connection!")
+                }
+            }
+
+            override fun onResponse(call: okhttp3.Call?, response: Response?) {
+                val message = response?.body()?.string()
+                Log.d(tag, "Message received $message")
+                this@LoginActivity.runOnUiThread {
+                    if (message != "[]" && message != "0") {
+                        val intent = Intent(this@LoginActivity, GameActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        longToast("Login Failed")
+                    }
+                }
+            }
+        })
     }
 }
