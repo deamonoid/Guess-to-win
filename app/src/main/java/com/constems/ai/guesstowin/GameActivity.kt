78acf169d7d72f94fde92762548a745d5182cd48
Tanmay Srivastava <tansrivastava@gmail.com>
2018-07-10 16:20:52 +0530

    Initial game activity code

--- app/src/main/java/com/constems/ai/guesstowin/GameActivity.kt
+++ app/src/main/java/com/constems/ai/guesstowin/GameActivity.kt
@@ -2,6 +2,7 @@ package com.constems.ai.guesstowin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
@@ -11,5 +12,14 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
 
         setSupportActionBar(toolbar_gameActivity)
+
+        val builder = intent.extras
+        val opponentName = builder.getString("opponent_name", "")
+        val myFirebaseId = builder.getString("my_firebase_id", "")
+        val playerFirebaseId = builder.getString("player_firebase_id", "")
+
+        textView_game_opponentName.text = opponentName.capitalize()
+
+        Log.v("GameActivity", "FirebaseID: $myFirebaseId, PlayerFirebaseId: $playerFirebaseId")
     }
 }
